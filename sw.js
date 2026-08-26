1f8374deb3e3c819355461fb51d89880642d18fa
DimonLSV <158223036+DimonLSV@users.noreply.github.com>
2026-08-26 10:40:03 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE='razryad-2026-08-26';
+const FILES=['./','./index.html','./chpu.html','./termist.html',
+ './manifest-chpu.webmanifest','./manifest-termist.webmanifest',
+ './icon-chpu-192.png','./icon-chpu-512.png','./icon-termist-192.png','./icon-termist-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))
+    .then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+    if(res&&res.status===200&&res.type==='basic'){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+    return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
